4311276368d0e3361b645fe19af3f7755eccfc61
camillejade <itookthisusername@live.com>
2015-02-02 16:02:03 +0800

    db
    sample db

---
+++ camzlibrary.sql
@@ -0,0 +1,85 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.6
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2015 at 09:00 AM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `camzlibrary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE IF NOT EXISTS `books` (
+  `bookid` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(30) NOT NULL,
+  `author` varchar(30) NOT NULL,
+  `pubdate` date NOT NULL,
+  `availability` varchar(5) NOT NULL,
+  `copies` int(3) NOT NULL,
+  PRIMARY KEY (`bookid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrowlog`
+--
+
+CREATE TABLE IF NOT EXISTS `borrowlog` (
+  `id` int(15) NOT NULL,
+  `username` int(15) NOT NULL,
+  `title` int(30) NOT NULL,
+  `duedate` date NOT NULL,
+  `status` varchar(15) NOT NULL,
+  `fine` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `fname` varchar(30) NOT NULL,
+  `mname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `usertype` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `fname`, `mname`, `lname`, `address`, `contact`, `usertype`) VALUES
+(1, 'admin', 'admin', 'admin', 'admin', 'admin', '1234 admin st.', '12345678901', 'admin'),
+(2, 'camz', 'camz', 'camille', 'aguihon', 'alcantara', '1234', '12345678901', 'user'),
+(3, 'jade', 'jade', 'jade', 'asd', 'asd', 'qweq', '123', 'librarian');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
