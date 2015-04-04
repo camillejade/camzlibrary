c05184f74e85861dfd0880dab9610dbf15b5b971
camillejade <itookthisusername@live.com>
2015-04-04 16:17:27 +0800

    working borrow module

--- borrowBook.php
+++ borrowBook.php
@@ -2,12 +2,23 @@
 include('session.php');
 include('connect.php');
 $id= $_GET['id'];
-$sql = "SELECT * from users WHERE id = '$id'";
 $bookid = $_GET['bookid'];
-$sql2 = "SELECT * from books where bookid = '$bookid'";
  
- echo "Hello Philippines and Hello World!";
+$sql3 = "SELECT * FROM borrowlog where userid = '$id' AND bookid = '$bookid'";
+$result = mysql_query($sql3);
 
+if(mysql_num_rows($result)>0)
+{
+	echo "Record exists";
+	echo "<br><a href='librarianhome.php'>Return to home</a>";
+}
+else
+{
+	$sql4 = "insert into borrowlog(userid, bookid, duedate) values ($id, $bookid, now())";
+	$result2 = mysql_query($sql4);
+	echo "<a href='librarianhome.php'>Return to home</a>";
+}
+ //insert into 
 //INSERT SA TABLE NA BORROWLOGS
 //UPDATE COUNT SA BOOKS
 
